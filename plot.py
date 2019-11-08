6a0d797ececde92e90f9696e8e27083f647b1fd4
Nisarg Shah <nisargs@cs.wisc.edu>
2019-11-07 22:46:45 -0600

    plots

--- plot.py
+++ plot.py
@@ -18,8 +18,8 @@ with open(sys.argv[1],'r') as file:
         i += 1
 
 plt.plot(x,y,',b')
-plt.xlabel('Time')
-plt.ylabel('tid')
+plt.xlabel('Buffer indices [capacity: 200 million]')
+plt.ylabel('Thread number')
 plt.title('Scheduling Distribution')
 plt.show()
 plt.savefig(sys.argv[1]+'.png')
